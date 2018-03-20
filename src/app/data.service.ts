e8f96f1ad38397623214ed9a4aa2294c24d13ff8
iposton <ianposton@sbcglobal.net>
2018-03-20 09:07:17 -0700

    remove lindgren and desmith from active roster

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse, HttpHeaders, HttpRequest } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
+
 
 
 let thisDate = new Date();
